7ea367cb69a6acdc7acb20777d30603e8d75600e
Michael Mcmanus <mikemcmanus95@gmail.com>
2018-12-13 23:33:32 -0500

    Initial Commit

---
+++ TipMate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipMate
+//
+//  Created by Michael Mcmanus on 12/13/18.
+//  Copyright © 2018 Michael Mcmanus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+

361adcba21826cae4f14a2bf7e69d5386440aaab
Michael Mcmanus <mike@Michaels-MBP.fios-router.home>
2018-12-15 02:11:16 -0500

    Here is my commit message

---
+++ TipMate/SettingsViewController.swift
@@ -0,0 +1,45 @@
+//
+//  SettingsViewController.swift
+//  TipMate
+//
+//  Created by Michael Mcmanus on 12/14/18.
+//  Copyright © 2018 Michael Mcmanus. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+  // @IBAction func setDefault(_ sender: Any) {
+       // let defaults = UserDefaults.resetStandardUserDefaults()
+
+   // let array = [0.18, 0.20, 0.25]
+ //   UserDefaults.standard.set(array, forKey: "SavedArray")
+    
+    
+    
+    
+    
+    
+    
+        
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+//}
--- TipMate/ViewController.swift
+++ TipMate/ViewController.swift
@@ -10,11 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabe: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var totalLabel2: UILabel!
+    @IBOutlet weak var totalLabel3: UILabel!
+    @IBOutlet weak var totalLabel4: UILabel!
+    
+    
+    
+    let vc = UIViewController()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        billField.becomeFirstResponder()
+        
     }
 
+    @IBAction func onTap(_ sender: Any) {
+        
+        view.endEditing(true)
+        
+    }
+    
+    @IBAction func calcTip(_ sender: Any) {
+        
+        let tipPercentages = [0.18, 0.2, 0.25]
+        
+        let bill = Double(billField.text!) ?? 0
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        let total2 = (bill + tip) / 2
+        let total3 = (bill + tip) / 3
+        let total4 = (bill + tip) / 4
+        
+    
+        
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabe.text = String(format: "$%.2f", total)
+        totalLabel2.text = String(format: "$%.2f", total2)
+        totalLabel3.text = String(format: "$%.2f", total3)
+        totalLabel4.text = String(format: "$%.2f", total4)
+    }
+    
+    
 
 }
 
